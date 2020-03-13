edece495a311d4dbbd6a0f7b25a6ba917802a379
EvgeniaChatzi <chatzievgenia7@gmail.com>
2020-03-13 11:49:23 +0100

    adding add movie modal functionalities

--- dom-06-demo-project-setup/assets/scripts/app.js
+++ dom-06-demo-project-setup/assets/scripts/app.js
@@ -6,6 +6,10 @@ const backdrop = document.querySelector("#backdrop");
 
 const cancelModalButton = addMovieModal.querySelector(".btn--passive");
 
+const addModalButton = cancelModalButton.nextElementSibling;
+
+const userInputs = addMovieModal.querySelectorAll("input");
+
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle("visible");
     toggleBackdrop();
@@ -20,8 +24,23 @@ addMovieModal.classList.remove("visible");
 backdrop.classList.remove("visible");
 }
 
+const addMoviewModalHandler = () => {
+const titleValue = userInputs[0].value;
+const imageUrlValue = userInputs[1].value;
+const ratingValue = userInputs[2].value;
+
+if (titleValue.trim() === "" || 
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5) {
+        alert("Please enter valid values (rating between 1 and 5)")
+    }
+}
+
 startAddMovieButton.addEventListener("click", toggleMovieModal);
 // startAddMovieButton.addEventListener("click", toggleBackdrop);
 cancelModalButton.addEventListener("click", closeModal);
 backdrop.addEventListener("click", closeModal);
+addModalButton.addEventListener("click", addMoviewModalHandler)
 
